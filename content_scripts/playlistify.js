9da4c92e7b1dfead021212cde8c4b65e3896365e
dazmy <adamzamzam571@gmail.com>
2025-06-13 10:10:35 +0700

    feat: simple play pause checking

--- content_scripts/playlistify.js
+++ content_scripts/playlistify.js
@@ -1,1 +1,8 @@
+const playPauseButton = document.getElementById("play-pause-button");
+const isPlay = playPauseButton.getAttribute("title").includes("Jeda");
 
+if (isPlay) {
+  console.log("SUDAH JALAN DARI TADI");
+} else {
+  playPauseButton.click();
+}
