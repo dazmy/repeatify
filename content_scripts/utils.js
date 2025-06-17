7c85442757d8cfc344b14902e51c952b894865cf
dazmy <adamzamzam571@gmail.com>
2025-06-17 09:17:45 +0700

    feat: refactor code for observer

--- content_scripts/utils.js
+++ content_scripts/utils.js
@@ -5,3 +5,27 @@ const optObsVideo = {
   attributeOldValue: true,
   attributeFilter: ["src"],
 };
+
+function checkStorage(video) {
+  browser.storage.local.get(["repeatRecent", "loop"], (data) => {
+    // later...
+    // repeatRecent(data.repeatRecent || 0);
+    video.loop = data.loop || false;
+  });
+}
+
+function obsChangeSong(video) {
+  const observeChangeSong = new MutationObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.oldValue) {
+        checkStorage(video);
+      }
+    });
+  });
+
+  observeChangeSong.observe(video, {
+    attributes: true,
+    attributeOldValue: true,
+    attributeFilter: ["src"],
+  });
+}
